b34e4f22b70d1ae6475a497c5f7adc246ce16963
Rommel Santos <rommelrenne@gmail.com>
2022-10-01 16:28:15 -0300

    Start qr code cration field

--- src/Components/creator/QRCreator.js
+++ src/Components/creator/QRCreator.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { useState } from "react";
+
+import QRCode from "react-qr-code";
+import QRCodeLink from "qrcode";
 
 import "./QRCreator.css";
 
 export default function QRCreator() {
-  return <h1>Ola</h1>;
+  const [urlInput, setUrlInput] = useState("");
+  const [qrCodeURL, setQRCodeURL] = useState("");
+
+  function generateQrCode(url) {
+    QRCodeLink.toDataURL(
+      url,
+      {
+        width: 500,
+        margin: 3,
+      },
+      function (err, url) {
+        setQRCodeURL(url);
+      }
+    );
+  }
+
+  function setQRCode(event) {
+    setUrlInput(event.target.value);
+    generateQrCode(event.target.value);
+  }
+
+  return (
+    <div className="app">
+      <QRCode value={urlInput} />
+
+      <input
+        placeholder="Insira o link aqui"
+        className="user-input"
+        value={urlInput}
+        onChange={(e) => setQRCode(e)}
+      />
+
+      <a href={qrCodeURL} download="qr-code.png">
+        Download
+      </a>
+    </div>
+  );
 }
