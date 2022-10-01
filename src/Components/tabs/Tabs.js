468c64fcf9c9c92545ef4134023374b1cadffb3f
Rommel Santos <rommelrenne@gmail.com>
2022-10-01 18:43:26 -0300

    Set tab navigation

--- src/Components/tabs/Tabs.js
+++ src/Components/tabs/Tabs.js
@@ -10,11 +10,27 @@ import QRScanner from "../scanner/QRScanner";
 export default function Tabs() {
   const [activeTab, setActiveTab] = useState("tab1");
 
+  const handleTab1 = () => {
+    setActiveTab("tab1");
+  };
+
+  const handleTab2 = () => {
+    setActiveTab("tab2");
+  };
+
   return (
     <div className="tabs">
       <ul className="nav">
-        <li className={activeTab === "tab1" ? "active" : ""}>Criar QR Code</li>
-        <li className={activeTab === "tab2" ? "active" : ""}>
+        <li
+          className={activeTab === "tab1" ? "active" : ""}
+          onClick={handleTab1}
+        >
+          Criar QR Code
+        </li>
+        <li
+          className={activeTab === "tab2" ? "active" : ""}
+          onClick={handleTab2}
+        >
           Scanear QR Code
         </li>
       </ul>
